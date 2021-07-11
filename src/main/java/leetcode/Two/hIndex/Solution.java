a0b1318df7f648a318b73820344a81636c081bb2
EarWheat <2636965138@qq.com>
2021-07-11 23:23:37 +0800

    h指数

---
+++ src/main/java/leetcode/Two/hIndex/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.Two.hIndex;
+
+/**
+ * @author liuzhaoluliuzhaolu
+ * @date 2021/7/11 下午11:22
+ * @desc
+ * @prd
+ * @Modification History:
+ * Date         Author          Description
+ * ------------------------------------------ *
+ */
+public class Solution {
+    public int hIndex(int[] citations) {
+
+    }
+}
